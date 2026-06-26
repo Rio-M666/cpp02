cc76a1d8c18bea56460b3a5c8e6a9db8612aee55
Miyachi Rio <mrio@c6r2s1.42tokyo.jp>
2026-06-26 17:54:12 +0900

    save

--- ex02/Fixed.hpp
+++ ex02/Fixed.hpp
@@ -19,6 +19,26 @@ class Fixed
 	int getRawBits(void) const;
 	void setRawBits(int const raw);
 
+	bool operator>(const Fixed &other) const;
+	bool operator<(const Fixed &other) const;
+	bool operator>=(const Fixed &other) const;
+	bool operator<=(const Fixed &other) const;
+	bool operator==(const Fixed &other) const;
+	bool operator!=(const Fixed &other) const;
+
+	Fixed operator+(const Fixed &other) const;
+	Fixed operator-(const Fixed &other) const;
+	Fixed operator*(const Fixed &other) const;
+	Fixed operator/(const Fixed &other) const;
+	Fixed &operator++();
+	Fixed operator++(int);
+	Fixed &operator--();
+	Fixed operator--(int);
+
+	static Fixed &min(Fixed &a, Fixed &b);
+	static const Fixed &min(const Fixed &a, const Fixed &b);
+	static Fixed &max(Fixed &a, Fixed &b);
+	static const Fixed &max(const Fixed &a, const Fixed &b);
 	float toFloat(void) const;
 	int toInt(void) const;
 };
